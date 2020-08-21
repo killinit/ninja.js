8b02906550bc05436fc9d6436b961b8bf2d44b36
Maciej Adamczewski <zapalniczek@gmail.com>
2020-08-21 11:06:08 +0200

    Pagination refactor

--- src/DataTable/Pagination.jsx
+++ src/DataTable/Pagination.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import Page from './Page'
+import Page from './Page';
 
-const Pagination = ({ currentPageNumber, totalNumberOfPages, onChange }) => {
-  const pages =
-    Array
-      .from(Array(totalNumberOfPages).keys())
-      .map(pageNumber => {
-        return <Page
-          key={pageNumber}
-          currentPageNumber={currentPageNumber}
-          pageNumber={pageNumber}
-          onChange={onChange} />
-      })
+const Pagination = ({ currentPageNumber, totalNumberOfPages, onChange }) => (
+  (totalNumberOfPages >= 1) &&
+  <ul className="pagination">
+    {Array.from(Array(totalNumberOfPages).keys()).map( pageNumber => (
+      <Page
+        key={pageNumber}
+        currentPageNumber={currentPageNumber}
+        pageNumber={pageNumber}
+        onChange={onChange}
+      />
+    ))}
+  </ul>
+);
 
-  if (pages.length <= 1) {
-    return null
-  }
-  return(
-    <ul className="pagination">
-      {pages}
-    </ul>
-  )
-}
+Pagination.propTypes = {
+  currentPageNumber: PropTypes.number.isRequired,
+  totalNumberOfPages: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired
+};
 
-export default Pagination
+export default Pagination;
