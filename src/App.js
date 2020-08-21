182b39ca2b1e1ccbc7c952e024ef3e0ab2689778
Maciej Adamczewski <zapalniczek@gmail.com>
2020-08-21 10:36:25 +0200

    App.js refactor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import DataTable from './DataTable';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container mt-3">
-        <DataTable rows={this.props.rows} locale="da" rowsPerPage={5} />
-      </div>
-    );
-  }
+const App = ({ rows }) => (
+  <div className="container mt-3">
+    <DataTable rows={rows} locale="da" rowsPerPage={5} />
+  </div>
+)
+
+App.propTypes = {
+  rows: PropTypes.arrayOf(PropTypes.shape({
+    name1: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    edit_path: PropTypes.string.isRequired,
+    per_id: PropTypes.number.isRequired,
+  })).isRequired
 }
 
 export default App;
